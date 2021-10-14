5aed64a6572718c7a2947dc37b4bc8701613ac90
tdelgio <tomas.dg@me.com>
2021-10-13 20:50:50 -0500

    contact logo bkg

--- src/components/DataSection/Form/ContactForm.js
+++ src/components/DataSection/Form/ContactForm.js
@@ -3,21 +3,22 @@ import { Link } from "gatsby"
 
 import Footer from "../../Footer"
 import { InstagramIcon } from "../../Icons"
+import { StaticImage } from "gatsby-plugin-image"
 
 const ContactForm = () => {
   return (
     <div className="bg-black h-screen">
       return (
       <>
-        <div className="lg:-mt-16 mt-16 flex flex-col items-center justify-center h-full min-h-screen tracking-widest leading-6 bg-black text-gray-200">
-          <div className="flex items-center justify-center flex-col mx-1 py-5  sm:py-10 tracking-widest border-2 border-gray-dark rounded-lg shadow-lg lg:p-8">
-            <div className="flex flex-col items-center md:flex-row align-center justify-center text-center xl:text-left sm:px-2 md:px-5 max-w-2xl">
+        <div className="lg:relative top-0 lg:top-12 bottom-0 lg:-mt-16 mt-16 flex flex-col items-center justify-center h-full min-h-screen tracking-widest leading-6 bg-black text-gray-200">
+          <div className="flex items-center justify-center flex-col mx-1 py-5  sm:py-10 tracking-widest border-2 bg-gray-dark  border-gray-dark rounded-lg shadow-lg lg:p-8">
+            <div className="flex flex-col items-center md:flex-row align-center justify-center bg-gray-dark text-center xl:text-left sm:px-2 md:px-5 max-w-2xl">
               <p className="mx-9 font-extralight text-lg tracking-widest leading-6">
                 ENVIA TU CONSULTA{" "}
               </p>
             </div>
             <div className="w-full flex flex-col py-4  z-30  space-y-6 max-w-2xl ">
-              <form className="w-full flex flex-col px-6 space-y-8 py-4 text-gray-dark">
+              <form className="w-full flex flex-col px-6 space-y-4 py-4 text-gray-dark">
                 <label>
                   <input
                     className="input"
@@ -49,6 +50,16 @@ const ContactForm = () => {
           <div className="lg:hidden mx-auto pt-12">
             <InstagramIcon />
           </div>
+        </div>
+        <div className="absolute top-0 h-screen w-screen flex items-center justify-center">
+          <StaticImage
+            height={1563}
+            src="../../../images/logo.png"
+            alt="image"
+            placeholder="tracedSVG"
+            layout="constrained"
+            className="transform scale-75 z-0 opacity-20 mx-auto"
+          />
         </div>
       </>
       <Footer />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <footer className="relative bottom-0 w-full bg-black text-center text-gray-300 font-light tracking-wide py-2 lg:py-2 text-xs lg:text-sm">
+    <footer className="fixed bottom-0 w-full bg-black text-center text-gray-300 font-light tracking-wide py-2 lg:py-2 text-xs lg:text-sm">
       © {new Date().getFullYear()}, DANOBIS SANDOVAL TATTOO
     </footer>
   )
